e68b9b4ee90318a21faeada964ca861c8fe807fd
Alina2495 <48183610+Alina2495@users.noreply.github.com>
2021-04-21 18:10:58 +0300

    21.04.2021

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-var messageBtn = document.getElementsByClassName('send-message');
-var message1 = document.getElementsByClassName('message');
-
-messageBtn.onclick = function() {
-    message1.classList.remove('d-none');
-};
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+});
+var galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+        swiper: galleryThumbs
+    }
+});
